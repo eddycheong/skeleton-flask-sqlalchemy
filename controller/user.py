02acbe353d7a60e10b8818ddc51b6c289e9aac4d
Eddy Cheong <cheong.e49@gmail.com>
2019-02-07 23:58:11 -0800

    Added marshmallow schema code to serialize sqlalchemy objects to json

--- controller/user.py
+++ controller/user.py
@@ -1,22 +1,24 @@
+from flask import jsonify
 from controller import api, session
 from database.model import User
+from database.schema import user
 
 @api.route("/user")
 def get_users():
-    #users = session.query(User).all()
-    ##print(users)
-    #print(users[0])
-    #print("hello")
-    return "Get all users"
+    user_schema = user.UserSchema(strict=True, many=True)
+
+    users = session.query(User).all()
+    results = user_schema.dump(users)
+    return jsonify(results)
 
 @api.route("/user", methods=["POST"])
 def create_user():
     return "Create user"
 
-@api.route("/user/<uuid:user_id>", methods=["PUT"])
+@api.route("/user/<int:user_id>", methods=["PUT"])
 def update_user(user_id):
     return f"Update user {user_id}"
 
-@api.route("/user/<uuid:user_id>", methods=["DELETE"])
+@api.route("/user/<int:user_id>", methods=["DELETE"])
 def delete_user(user_id):
     return f"Deleting user {user_id}"
