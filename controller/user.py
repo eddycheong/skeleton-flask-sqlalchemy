b7545bfbd5c87de1f9ba1dae2007e08cf4b895d3
Eddy Cheong <cheong.e49@gmail.com>
2019-02-08 00:19:36 -0800

    Working implementation of creating user example

--- controller/user.py
+++ controller/user.py
@@ -1,19 +1,31 @@
-from flask import jsonify
+from flask import request, jsonify
 from controller import api, session
 from database.model import User
 from database.schema import user
 
 @api.route("/user")
 def get_users():
-    user_schema = user.UserSchema(strict=True, many=True)
+    users_schema = user.UserSchema(strict=True, many=True)
 
     users = session.query(User).all()
-    results = user_schema.dump(users)
+    results = users_schema.dump(users)
     return jsonify(results)
 
 @api.route("/user", methods=["POST"])
 def create_user():
-    return "Create user"
+    user_schema = user.UserSchema(strict=True, many=False)
+    user_data = request.get_json()
+
+    data = user_schema.load(user_data).data    
+    print(data)
+    new_user = User(name=data["name"])
+    session.add(new_user)
+    session.commit()
+    session.refresh(new_user)
+    
+    result = user_schema.dump(new_user)
+
+    return jsonify(result)
 
 @api.route("/user/<int:user_id>", methods=["PUT"])
 def update_user(user_id):
