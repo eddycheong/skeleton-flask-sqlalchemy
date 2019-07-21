ab3de28ca223d56eb0af8f46963a475e5bd3937e
Eddy Cheong <cheong.e49@gmail.com>
2019-07-21 10:30:10 -0700

    Clean up seeder.py

--- seeder.py
+++ seeder.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
-from database import Base, Engine
 from database.seed import Seeder
 
-# Create database tables
-Base.metadata.create_all(Engine)
-
-# Creates all of the tables
+# Creates seed data
 seeder = Seeder()
 seeder.seed()
