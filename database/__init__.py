a576c26ea0c51e3f39dd4b271ceea74fcf9743e1
Eddy Cheong <cheong.e49@gmail.com>
2019-07-21 00:57:12 -0700

    Moving SQLAlchemy base from database to database.model. Adding alembic and working auto migration

--- database/__init__.py
+++ database/__init__.py
@@ -1,11 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
 from utils import JsonConfigParser
 
 database_config = JsonConfigParser().config["database"]
 
-Base = declarative_base()
 Engine = create_engine(database_config["connection_url"], connect_args={'check_same_thread': False})
 Session = sessionmaker(bind=Engine)
 
--- database/model/__init__.py
+++ database/model/__init__.py
@@ -1,1 +1,4 @@
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
 from .user import User
